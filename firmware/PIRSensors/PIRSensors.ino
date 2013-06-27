f8a2b012ec39b6e81dbe83acf214009186749445
Marius Kintel <marius@kintel.net>
2013-06-26 22:37:18 -0400

    PIR sensor firmware

---
+++ firmware/PIRSensors/PIRSensors.ino
@@ -0,0 +1,33 @@
+// Yep, this is actually -*- c++ -*-
+
+#define PIR_START_PIN 3
+#define NUM_PIR_SENSORS 6
+
+bool buffer[NUM_PIR_SENSORS];
+
+void setval(uint8_t idx, bool val)
+{
+  buffer[idx] = val;
+  Serial.print(idx);
+  Serial.print(" ");
+  Serial.println(val);
+}
+
+void setup()
+{
+  Serial.begin(57600);
+
+  for (uint8_t i=0; i<NUM_PIR_SENSORS; i++) {
+    pinMode(PIR_START_PIN + i, INPUT);
+    digitalWrite(PIR_START_PIN + i, 1); // Enable internal pull-up
+    setval(i, false);
+  }
+}
+
+void loop()
+{
+  for (uint8_t i=0; i<NUM_PIR_SENSORS; i++) {
+    bool val = !digitalRead(PIR_START_PIN + i);
+    if (buffer[i] != val) setval(i, val);
+  }
+}
