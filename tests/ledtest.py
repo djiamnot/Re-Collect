2b5c92739825604d775db36e66c30524b11f00d3
Michal Seta <djiamnot@gmail.com>
2013-06-28 19:51:19 -0400

    test on linux

--- tests/ledtest.py
+++ tests/ledtest.py
@@ -6,7 +6,7 @@ import time
 import math
 
 # Arduino in the box
-serialport = '/dev/tty.usbserial-A40014iF'
+serialport = '/dev/ttyUSB0'
 # Development Arduino
 #serialport = '/dev/tty.usbmodemfa141'
 
