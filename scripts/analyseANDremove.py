0855cf40fadd56dc322cd6e6ef73bd4bafb32861
Michal Seta <djiamnot@gmail.com>
2015-11-05 23:21:52 -0500

    fix the sound discarting script

--- scripts/analyseANDremove.py
+++ scripts/analyseANDremove.py
@@ -5,10 +5,14 @@ import re
 import logging
 import subprocess as sp
 
+# DIR = "/home/mis/src/Re-Collect/snd"
 DIR = "/home/mis/src/Re-Collect/snd"
+DESTDIR = "/home/mis/src/Re-Collect/snd-reject"
 FILES = []
-file = "/home/mis/src/Re-Collect/snd/20150421-10-57-21.wav"
-
+#file = "/home/mis/src/Re-Collect/snd/20150421-10-57-21.wav"
+file = ""
+minAmp = 1600
+maxAmp = 29000
 logging.basicConfig(filename="/var/tmp/ReCollect.log", format='%(asctime)s %(message)s', level=logging.DEBUG)
 
 def listFiles(dir):
@@ -17,8 +21,8 @@ def listFiles(dir):
     files = sp.check_output(["ls", dir])
     for f in files.split("\n"):
         absolute = os.path.join(DIR, f)
-        if len(file) > 0:
-            if os.stat(absolute).st_size == 0:
+        if len(f) > 0:
+            if os.stat(absolute).st_size < 100:
                 os.remove(absolute)
                 logging.debug("Removed {}".format(absolute))
             else:
@@ -39,9 +43,11 @@ def sndfile(dir):
             sigData = sigData[1].strip()
             sigData = sigData.split(" ")[0]
             loudness = int(sigData)
-            #logging.info("file: {} - loudness: {}".format(os.path.split(file)[1], loudness))
-            if loudness < 100:
+            # logging.debug("file: {} - loudness: {}".format(os.path.split(file)[1], loudness))
+            if loudness < minAmp or loudness > maxAmp:
+                filename = os.path.split(file)[1]
                 logging.warning("-------> Removing file: {} with loudness {}".format(os.path.split(file)[1], loudness))
+                os.rename(file, os.path.join(DESTDIR, filename))
         except sp.CalledProcessError:
             logging.error("error occured")
             pass
